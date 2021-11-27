14d7cf8fa10ad1476917a22741e8a59b10414999
Jose Hernandez <pepeh93@hotmail.es>
2021-11-27 19:59:39 +0100

    Created restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :restaurants
+  # # READ
+  # # Get all restaurants
+  # get 'restaurants', to: 'restaurants#index', as: :restaurants
+  # # Get one restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # CREATE
+  # # To creates content we need 2 actions
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # UPDATE
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  # # DELETE
+  # delete '/restaurants:id', to: 'restaurants#destroy'
 end
